6ed437c440372093912850015ea59dd21d8863c6
Ayonspoiler <ayon4052@gmail.com>
2025-09-09 13:53:19 +0600

    Small update for Registration form.

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -646,7 +646,6 @@ const Registration = () => {
                 name="dateOfEstablishment"
                 value={formData.dateOfEstablishment}
                 onChange={handleInputChange}
-                min={new Date().toISOString().split("T")[0]}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
               />
             </div>
